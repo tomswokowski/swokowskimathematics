957d8e848cb3ff88bebf6ab5fc445375d40ed2de
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-05-28 14:30:25 -0500

    Implement Basic Resource Page layout

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,7 +1,32 @@
+import SubscribeSection from '../components/common/SubscribeSection';
 import { MainLayout } from '../components/layouts/MainLayout';
 
 const Resources = () => {
-  return <h1>Resources Page</h1>;
+  return (
+    <>
+      <h1 className="mb-8 text-center text-6xl font-bold">Resources</h1>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto mb-16 max-w-3xl text-center">
+          <h2 className="mb-4 text-3xl font-semibold">Video Content</h2>
+          <p className="text-gray-500">
+            Videos are available on YouTube or Udemy.
+          </p>
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto mb-16 max-w-3xl text-center">
+          <h2 className="mb-4 text-3xl font-semibold">
+            Practice & Answer Workbook
+          </h2>
+          <p className="text-gray-500">
+            You can download the practice and answer workbook.
+          </p>
+        </div>
+      </div>
+
+      <SubscribeSection />
+    </>
+  );
 };
 
 Resources.layout = MainLayout;
