b599d34c279b5e4c119defc0e6a4c5e173ea1ffd
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-05-28 13:54:09 -0500

    Implement Lesson Page Layout

---
+++ data/courseData.ts
@@ -0,0 +1,78 @@
+export const courseData = [
+  {
+    number: '1',
+    lessons: [
+      {
+        number: '1',
+        slug: 'lesson1',
+        title: 'lesson1',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+      {
+        number: '2',
+        slug: 'lesson2',
+        title: 'lesson2',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+    ],
+  },
+  {
+    number: '2',
+    lessons: [
+      {
+        number: '3',
+        slug: 'lesson1',
+        title: 'lesson1',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+      {
+        number: '4',
+        slug: 'lesson2',
+        title: 'lesson2',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+    ],
+  },
+  {
+    number: '3',
+    lessons: [
+      {
+        number: '5',
+        slug: 'lesson1',
+        title: 'lesson1',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+      {
+        number: '6',
+        slug: 'lesson2',
+        title: 'lesson2',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+    ],
+  },
+  {
+    number: '4',
+    lessons: [
+      {
+        number: '1',
+        slug: 'lesson1',
+        title: 'lesson1',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+      {
+        number: '1',
+        slug: 'lesson2',
+        title: 'lesson2',
+        description: 'lesson1 description',
+        youtubeUrl: 'youtube.com',
+      },
+    ],
+  },
+];
--- pages/lessons/index.tsx
+++ pages/lessons/index.tsx
@@ -1,7 +1,46 @@
 import { MainLayout } from '../../components/layouts/MainLayout';
+import { courseData } from '../../data/courseData';
 
 const Lessons = () => {
-  return <h1>Lessons Page</h1>;
+  return (
+    <>
+      <h1 className="mb-8 text-center text-3xl font-bold">
+        Full Algebra Course Curriculum
+      </h1>
+
+      <div className="mb-20 flex justify-center">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-red-600 focus:ring-red-600 sm:text-sm md:w-1/2 xl:w-1/3"
+          placeholder="What are you looking to learn? Enter a search term..."
+        />
+      </div>
+
+      <div className="mx-2 mb-8 flex flex-wrap lg:px-8">
+        {courseData.map((chapter: any) => {
+          return (
+            <div key={chapter.number} className="mb-4 w-full px-2 md:w-1/3">
+              <h3 className="mb-4 text-center text-2xl font-semibold">
+                Chapter {chapter.number}
+              </h3>
+              {chapter.lessons.map((lesson: any) => {
+                return (
+                  <div
+                    key={lesson.number}
+                    className="text-grey-dark mb-4 flex h-12 items-center justify-center border text-sm"
+                  >
+                    <p>{lesson.title}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 Lessons.layout = MainLayout;
