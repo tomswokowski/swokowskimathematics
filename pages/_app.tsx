0f997df25723ea301146a672884be5b436e18b6a
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-05-29 07:31:35 -0500

    Integrate Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 import PageWithLayoutType from '../types/pageWithLayouts';
 import '../styles/globals.css';
+import Script from 'next/script';
 
 type AppLayoutProps = AppProps & {
   Component: PageWithLayoutType;
@@ -14,6 +15,22 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
 
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="ga-script">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+        `}
+      </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
