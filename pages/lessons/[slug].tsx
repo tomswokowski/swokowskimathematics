d1087647650ba501f082739d72eccb13fbb686e3
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-05-29 12:27:29 -0500

    small lesson page copy update

--- pages/lessons/[slug].tsx
+++ pages/lessons/[slug].tsx
@@ -49,7 +49,7 @@ const LessonPage = ({ lesson, previousSlug, nextSlug }: any) => {
                     className="mr-1 h-5 w-5 text-white"
                     aria-hidden="true"
                   />{' '}
-                  Previous Lesson
+                  Previous
                 </a>
               </Link>
             )}
@@ -57,7 +57,7 @@ const LessonPage = ({ lesson, previousSlug, nextSlug }: any) => {
             {nextSlug && (
               <Link href={`/lessons/${nextSlug}`}>
                 <a className="flex w-1/3 items-center justify-center rounded-md border border-transparent bg-red-600 py-2 px-1 text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 md:w-1/5">
-                  Next Lesson{' '}
+                  Next{' '}
                   <ArrowRightIcon
                     className="ml-1 h-5 w-5 text-white"
                     aria-hidden="true"
