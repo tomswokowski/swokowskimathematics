6248ff87f07639bca9ea5f3776350b3672ec68cf
Thomas Swokowski <thomasswokowski@Thomass-Mac-mini.local>
2022-05-29 17:19:50 -0500

    Implement 404 and 500 error pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,22 +1,22 @@
 import { MainLayout } from '../components/layouts/MainLayout';
 import Link from 'next/link';
 
-export default function PageNotFound() {
+export default function InternalServerError() {
   return (
     <>
       <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div className="mx-auto max-w-max">
           <main className="sm:flex">
             <p className="text-4xl font-extrabold text-red-600 sm:text-5xl">
-              404
+              500
             </p>
             <div className="sm:ml-6">
               <div className="sm:border-l sm:border-gray-200 sm:pl-6">
                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
-                  Page not found
+                  Internal Server Error
                 </h1>
                 <p className="mt-1 text-base text-gray-500">
-                  Please check the URL in the address bar and try again.
+                  We are sorry, an error has occurred processing your request.
                 </p>
               </div>
               <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
@@ -39,4 +39,4 @@ export default function PageNotFound() {
   );
 }
 
-PageNotFound.layout = MainLayout;
+InternalServerError.layout = MainLayout;
